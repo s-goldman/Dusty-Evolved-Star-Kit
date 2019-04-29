752b38d84dc5531cde95ef31275a7a92d395b7e3
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2019-04-29 14:22:07 -0400

    Update __init__.py

--- desk/__init__.py
+++ desk/__init__.py
@@ -22,4 +22,3 @@ if sys.version_info < tuple((int(val) for val in __minimum_python_version__.spli
 if not _ASTROPY_SETUP_:
     # For egg_info test builds to pass, put package imports here.
     from .example_mod import *
-    from python_scripts import plotting_seds
