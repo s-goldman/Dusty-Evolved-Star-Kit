12eb646e660ea7d988c5f20cc291d93bc5cbfd84
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2019-04-29 16:38:07 -0400

    added logo

--- docs/conf.py
+++ docs/conf.py
@@ -92,6 +92,10 @@ html_theme = 'sphinx_rtd_theme'
 #
 # html_theme_options = {}
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = 'the_desk.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
