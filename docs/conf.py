c856a92b321008a523e4d70481c5ee77926c168e
Steve Goldman <sgoldman@stsci.edu>
2019-05-13 19:36:04 -0400

    changed travis file to build_spinx

--- docs/conf.py
+++ docs/conf.py
@@ -22,8 +22,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import desk
-import numpy
+# import desk
+
 
 # -- General configuration ---------------------------------------------
 
