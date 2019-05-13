158fc6ee39cbdbef4a2fd7c16a6726a3f447935f
Steve Goldman <sgoldman@stsci.edu>
2019-05-13 18:34:13 -0400

    changed travis file to build_spinx

--- docs/conf.py
+++ docs/conf.py
@@ -143,7 +143,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'desk',
+    (master_doc, 'dusty_evolved_star_kit',
      u'Dusty-Evolved-Star-Kit Documentation',
      [author], 1)
 ]
@@ -155,10 +155,10 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'desk',
+    (master_doc, 'dusty_evolved_star_kit',
      u'Dusty-Evolved-Star-Kit Documentation',
      author,
-     'desk',
+     'dusty_evolved_star_kit',
      'One line description of project.',
      'Miscellaneous'),
 ]
