7bd643d5ea9fc709cee2746ef3404fe03790dfc6
Steve Goldman <sgoldman@stsci.edu>
2019-08-01 15:06:48 -0400

    fixed console scripts

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/s-goldman/Dusty_Evolved_Star_Kit',
-    version='1.5.2',
+    version='1.5.3',
     zip_safe=False,
 )
