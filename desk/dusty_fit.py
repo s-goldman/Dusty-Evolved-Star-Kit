f556b2e54d7e31700d297795bba7814453202ffa
Steve Goldman <sgoldman@stsci.edu>
2020-01-13 11:21:44 -0500

    Added documentation to dusty_fit

--- desk/dusty_fit.py
+++ desk/dusty_fit.py
@@ -1,4 +1,5 @@
 import csv
+import ipdb
 import copy
 import math
 import numpy as np
@@ -8,6 +9,32 @@ from desk import sed_fit, config
 def dusty_fit(
     source, distance, model_grid, grid_dusty, grid_outputs, counter, number_of_targets
 ):
+    """Function fits astropy table data with a least squares method.
+
+    Parameters
+    ----------
+    source : str
+        Name of the source being fit.
+    distance : float
+        Distance to source in kpc.
+    model_grid : str
+        Name of model grid being used.
+    grid_dusty : astropy table
+        Table with two items in each row item 1 being an array
+        with wavelength in microns and item 2 being an array with flux in w/m2.
+    grid_outputs : astropy table
+        Table with each row showing the output results corresponding to each row
+        in grid_dusty
+    counter : int
+        The nth item being fit.
+    number_of_targets : int
+        The total number of sources to be fit.
+
+    """
+
+    # Initialize variables
+    stat_values = []
+
     def make_output_files_dusty():
         with open("fitting_results.csv", "w") as f:
             f.write("source,L,vexp_predicted,teff,tinner,odep,mdot\n")
@@ -16,24 +43,30 @@ def dusty_fit(
             f.write("target_name,data_file,norm,index,grid_name,teff,tinner,odep\n")
             f.close()
 
+    # Creates output file
     make_output_files_dusty()
-    # calculates luminosity and scales outputs
-    stat_values = []
-    raw_data = sed_fit.get_data(source)  # gets target data
-    # model_x = grid_dusty[0][0][:]  # gets model wavelengths
+
+    # gets target data
+    raw_data = sed_fit.get_data(source)
+
     for model in np.array(grid_dusty):
-        trimmed_model = sed_fit.trim(
-            raw_data, model
-        )  # gets fluxes for corresponding wavelengths of data and models
+        # removes data outside of wavelegth range of model grid
+        trimmed_model = sed_fit.trim(raw_data, model)
+
+        # gets fluxes for corresponding wavelengths of data and models
         matched_model = sed_fit.find_closest(raw_data, trimmed_model)
+
+        # fits source with n(set in config) models spanning 4 orders of magnitude
         stats, trials = sed_fit.fit_norm(raw_data, matched_model)
         stat_values.append(stats)
+
     stat_array = np.vstack(stat_values)
-    argmin = np.argmin(stat_array)
+    argmin = np.argmin(stat_array)  # lowest chi square value
     model_index = argmin // stat_array.shape[1]
     trial_index = argmin % stat_array.shape[1]
     target_name = (source.split("/")[-1][:15]).replace("IRAS-", "IRAS ")
 
+    # normalize output values by the set distance
     distance_value = float(copy.copy(distance))
     distance_norm = math.log10(((float(distance) / 4.8482e-9) ** 2) / 1379)
     luminosity = int(
@@ -52,7 +85,7 @@ def dusty_fit(
     tinner = int(grid_outputs[model_index]["tinner"])
     odep = grid_outputs[model_index]["odep"]
 
-    # creates output file
+    # creates results file
     latex_array = [
         target_name,
         luminosity,
@@ -63,6 +96,7 @@ def dusty_fit(
         "%.3E" % float(scaled_mdot),
     ]
 
+    # creates file for creating figure
     plotting_array = [
         target_name,
         source,
@@ -99,6 +133,8 @@ def dusty_fit(
         print(("Expansion velocity (scaled)\t|\t" + str(round(scaled_vexp, 2))))
         print(("Gas mass loss (scaled)\t\t|\t" + str("%.2E" % float(scaled_mdot))))
         print("-------------------------------------------------")
+
+    # saves files
     with open("fitting_results.csv", "a") as f:
         writer = csv.writer(f, delimiter=",", lineterminator="\n")
         writer.writerow(np.array(latex_array))
