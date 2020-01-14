GRAMS scaling fixed

desk/grams_fit.py
@@ -1,6 +1,7 @@
 import csv
 import copy
 import math
+import ipdb
 import numpy as np
 from desk import sed_fit, config
 
@@ -34,37 +35,32 @@ def grams_fit(
     # Initialize variables
     stat_values = []
 
-    def make_output_files_grams():
-        with open("fitting_results.csv", "w") as f:
-            f.write("source,L,rin,teff,tinner,odep,mdot\n")
-            f.close()
-        with open("fitting_plotting_outputs.csv", "w") as f:
-            f.write("target_name,data_file,norm,index,grid_name,teff,tinner,odep\n")
-            f.close()
-
-    # Creates output file
-    if counter == 0:
-        make_output_files_dusty()
-
     # gets target data
     raw_data = sed_fit.get_data(source)
 
-    for model in np.array(grid_dusty):
+    #
+    flux_scaling_factor = 50 ** 2 / float(distance) ** 2
+
+    def trim_find_lsq(model):
         # removes data outside of wavelegth range of model grid
         trimmed_model = sed_fit.trim(raw_data, model)
 
         # gets fluxes for corresponding wavelengths of data and models
         matched_model = sed_fit.find_closest(raw_data, trimmed_model)
 
-        # fits source with n(set in config) models spanning 4 orders of magnitude
-        stats, trials = sed_fit.fit_norm(raw_data, matched_model)
+        # normalize model to specified distance
+        scaled_matched_model = matched_model * flux_scaling_factor
+
+        # fits source with least squares
+        stats = sed_fit.least2(raw_data, matched_model)
         stat_values.append(stats)
+
+    [trim_find_lsq(x) for x in grid_dusty]
 
     # obtains best fit model and model index
     stat_array = np.vstack(stat_values)
     argmin = np.argmin(stat_array)  # lowest chi square value
     model_index = argmin // stat_array.shape[1]
-    trial_index = argmin % stat_array.shape[1]
     target_name = (source.split("/")[-1][:15]).replace("IRAS-", "IRAS ")
 
     distance_value = float(copy.copy(distance))
@@ -89,10 +85,11 @@ def grams_fit(
     plotting_array = [
         target_name,
         source,
-        trials[trial_index],
+        flux_scaling_factor,
         model_index,
         model_grid,
         teff,
+        tinner,
         odep,
     ]
     if config.output["printed_output"] == "True":

desk/plotting_seds.py
@@ -1,22 +1,23 @@
 import math
-import pdb
+import ipdb
 
 import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
+from fnmatch import fnmatch
 from astropy.io import ascii
 from astropy.table import Table
 from desk import config
 from matplotlib import rc
 
-'''
+"""
 Steve Goldman
 Space Telescope Science Institute
 May 17, 2018
 sgoldman@stsci.edu
 
 This script is for plotting the outputs of the sed_fitting script.
-'''
+"""
 
 
 def create_fig():
@@ -29,23 +30,25 @@ def create_fig():
     # plt.rcParams['text.usetex'] = True
     # plt.rcParams['text.latex.unicode'] = True
 
-    full_path = str(__file__.replace('plotting_seds.py', ''))
-    input_file = Table.read('fitting_plotting_outputs.csv')
-    grid_dusty = Table.read(full_path + 'models/' + str(input_file['grid_name'][0]) + '_models.fits')
+    full_path = str(__file__.replace("plotting_seds.py", ""))
+    input_file = Table.read("fitting_plotting_outputs.csv")
+    grid_dusty = Table.read(
+        full_path + "models/" + str(input_file["grid_name"][0]) + "_models.fits"
+    )
 
     def get_data(filename):
         """
         :param filename: filename of input data. Should be csv with Column 0: wavelength in um and Col 1: flux in Jy
         :return: two arrays of wavelength (x) and flux (y) in unit specified in config.py
         """
-        table = ascii.read(filename, delimiter=',')
+        table = ascii.read(filename, delimiter=",")
         table.sort(table.colnames[0])
         x = np.array(table.columns[0])
         y = np.array(table.columns[1])
-        if config.output['output_unit'] == 'Wm^-2':
+        if config.output["output_unit"] == "Wm^-2":
             y = y * u.Jy
             y = y.to(u.W / (u.m * u.m), equivalencies=u.spectral_density(x * u.um))
-        elif config.output['output_unit'] == 'Jy':
+        elif config.output["output_unit"] == "Jy":
             pass
         else:
             raise ValueError("Unit in config.py not 'Wm^-2' or 'Jy'")
@@ -59,84 +62,117 @@ def create_fig():
     elif len(input_file) == 3:
         fig, axs = plt.subplots(3, 1, sharex=True, sharey=True, figsize=(8, 10))
     else:
-        fig, axs = plt.subplots(math.ceil(len(input_file) / 3), 3, sharex=True, sharey=True, figsize=(8, 10))
+        fig, axs = plt.subplots(
+            math.ceil(len(input_file) / 3), 3, sharex=True, sharey=True, figsize=(8, 10)
+        )
         axs = axs.ravel()
 
     for counter, target in enumerate(input_file):
         # gets data for plotting
-        target_name = (target['target_name']).replace('.csv', '').replace('_', ' ')
-        x_data, y_data = get_data(target['data_file'])
-        x_model, y_model = grid_dusty[target['index']]
+        target_name = (target["target_name"]).replace(".csv", "").replace("_", " ")
+        x_data, y_data = get_data(target["data_file"])
+        x_model, y_model = grid_dusty[target["index"]]
         x_model = x_model[np.where(y_model != 0)]
-        y_model = y_model[np.where(y_model != 0)] * input_file[counter]['norm']
-        if config.output['output_unit'] == 'Wm^-2':
-            axislabel = "log $\lambda$ F$_{\lambda}$ (W m$^{-2}$)"
-        elif config.output['output_unit'] == 'Jy':
-            y_model = y_model * u.W / (u.m * u.m)
-            y_model = y_model / ((x_model * u.um).to(u.Hz, equivalencies=u.spectral()))
-            y_model = y_model.to(u.Jy).value
-            axislabel = "log F$_{\lambda}$ (Jy)"
+        if fnmatch(input_file["grid_name"][0], "grams*"):
+            y_model = y_model * u.Jy
+            y_model = y_model.to(
+                u.W / (u.m * u.m), equivalencies=u.spectral_density(x_model * u.um)
+            )
         else:
-            raise ValueError("Unit in config.py not 'Wm^-2' or 'Jy'")
+            y_model = y_model * u.W / (u.m * u.m)
+        y_model = y_model[np.where(y_model != 0)] * input_file[counter]["norm"]
+        axislabel = "log $\lambda$ F$_{\lambda}$ (W m$^{-2}$)"
+
+        # if config.output["output_unit"] == "Wm^-2":
+        #     axislabel = "log $\lambda$ F$_{\lambda}$ (W m$^{-2}$)"
+        # elif config.output["output_unit"] == "Jy":
+        #     y_model = y_model * u.W / (u.m * u.m)
+        #     y_model = y_model / ((x_model * u.um).to(u.Hz, equivalencies=u.spectral()))
+        #     y_model = y_model.to(u.Jy).value
+        #     axislabel = "log F$_{\lambda}$ (Jy)"
+        # else:
+        #     raise ValueError("Unit in config.py not 'Wm^-2' or 'Jy'")
 
         # logscale
         x_model = np.log10(x_model)
-        y_model = np.log10(y_model)
+        y_model = np.log10(y_model.value)
         x_data = np.log10(x_data)
         y_data = np.log10(y_data)
 
-        # out = Table((x_model,y_model), names=('wave', 'lamflam'))
-        # out.write('crystalline_fit.csv', format='csv')
-        # pdb.set_trace()
-        # plotting
         if len(input_file) == 1:
             ax1.set_xlim(-0.99, 2.49)
             ax1.set_ylim(np.median(y_model) - 2, np.median(y_model) + 2)
-            ax1.scatter(x_data, y_data, c='blue', label='data')
-            ax1.plot(x_model, y_model, c='k', linewidth=0.5, linestyle='--', zorder=2, label='model')
-            ax1.annotate(target_name.replace('-', r'\textendash'), (0.07, 0.85), xycoords='axes fraction', fontsize=14)
-            ax1.get_xaxis().set_tick_params(which='both', direction='in', labelsize=15)
-            ax1.get_yaxis().set_tick_params(which='both', direction='in', labelsize=15)
-            ax1.set_xlabel('log $\lambda$ ($\mu m$)', labelpad=10)
+            ax1.scatter(x_data, y_data, c="blue", label="data")
+            ax1.plot(
+                x_model,
+                y_model,
+                c="k",
+                linewidth=0.5,
+                linestyle="--",
+                zorder=2,
+                label="model",
+            )
+            ax1.annotate(
+                target_name.replace("-", r"\textendash"),
+                (0.07, 0.85),
+                xycoords="axes fraction",
+                fontsize=14,
+            )
+            ax1.get_xaxis().set_tick_params(which="both", direction="in", labelsize=15)
+            ax1.get_yaxis().set_tick_params(which="both", direction="in", labelsize=15)
+            ax1.set_xlabel("log $\lambda$ ($\mu m$)", labelpad=10)
             ax1.set_ylabel("log $\lambda$ F$_{\lambda}$ " + "(W m$^{-2}$)", labelpad=10)
         else:
             axs[counter].set_xlim(-0.99, 2.49)
             axs[counter].set_ylim(np.median(y_model) - 2, np.median(y_model) + 2)
-            axs[counter].plot(x_model, y_model, c='k', linewidth=0.4, linestyle='--', zorder=2)
-            axs[counter].scatter(x_data, y_data, c='blue')
-            axs[counter].annotate(target_name.replace('-', r'\textendash'), (0.7, 0.8), xycoords='axes fraction',
-                                  fontsize=14)
-            axs[counter].get_xaxis().set_tick_params(which='both', direction='in', labelsize=15)
-            axs[counter].get_yaxis().set_tick_params(which='both', direction='in', labelsize=15)
-            axs[counter].set_xlabel('log $\lambda$ ($\mu m$)', labelpad=10)
+            axs[counter].plot(
+                x_model, y_model, c="k", linewidth=0.4, linestyle="--", zorder=2
+            )
+            axs[counter].scatter(x_data, y_data, c="blue")
+            axs[counter].annotate(
+                target_name.replace("-", r"\textendash"),
+                (0.7, 0.8),
+                xycoords="axes fraction",
+                fontsize=14,
+            )
+            axs[counter].get_xaxis().set_tick_params(
+                which="both", direction="in", labelsize=15
+            )
+            axs[counter].get_yaxis().set_tick_params(
+                which="both", direction="in", labelsize=15
+            )
+            axs[counter].set_xlabel("log $\lambda$ ($\mu m$)", labelpad=10)
             axs[counter].set_ylabel(axislabel, labelpad=10)
 
         # pdb.set_trace()
     plt.subplots_adjust(wspace=0, hspace=0)
-    fig.savefig('output_sed.png', dpi=200, bbox_inches='tight')
+    fig.savefig("output_sed.png", dpi=200, bbox_inches="tight")
+
 
 def single_fig():
     """
     :return: Runs plotting script
     """
 
-    full_path = str(__file__.replace('plotting_seds.py', ''))
-    input_file = Table.read('fitting_plotting_outputs.csv')
-    grid_dusty = Table.read(full_path + 'models/' + str(input_file['grid_name'][0]) + '_models.fits')
+    full_path = str(__file__.replace("plotting_seds.py", ""))
+    input_file = Table.read("fitting_plotting_outputs.csv")
+    grid_dusty = Table.read(
+        full_path + "models/" + str(input_file["grid_name"][0]) + "_models.fits"
+    )
 
     def get_data(filename):
         """
         :param filename: filename of input data. Should be csv with Column 0: wavelength in um and Col 1: flux in Jy
         :return: two arrays of wavelength (x) and flux (y) in unit specified in config.py
         """
-        table = ascii.read(filename, delimiter=',')
+        table = ascii.read(filename, delimiter=",")
         table.sort(table.colnames[0])
         x = np.array(table.columns[0])
         y = np.array(table.columns[1])
-        if config.output['output_unit'] == 'Wm^-2':
+        if config.output["output_unit"] == "Wm^-2":
             y = y * u.Jy
             y = y.to(u.W / (u.m * u.m), equivalencies=u.spectral_density(x * u.um))
-        elif config.output['output_unit'] == 'Jy':
+        elif config.output["output_unit"] == "Jy":
             pass
         else:
             raise ValueError("Unit in config.py not 'Wm^-2' or 'Jy'")
@@ -144,17 +180,16 @@ def single_fig():
 
     # setting axes
 
-
     for counter, target in enumerate(input_file):
         # gets data for plotting
-        target_name = (target['target_name']).replace('.csv', '')
-        x_data, y_data = get_data(target['data_file'])
-        x_model, y_model = grid_dusty[target['index']]
+        target_name = (target["target_name"]).replace(".csv", "")
+        x_data, y_data = get_data(target["data_file"])
+        x_model, y_model = grid_dusty[target["index"]]
         x_model = x_model[np.where(y_model != 0)]
-        y_model = y_model[np.where(y_model != 0)] * input_file[counter]['norm']
-        if config.output['output_unit'] == 'Wm^-2':
+        y_model = y_model[np.where(y_model != 0)] * input_file[counter]["norm"]
+        if config.output["output_unit"] == "Wm^-2":
             axislabel = "log $\lambda$ F$_{\lambda}$ (W m$^{-2}$)"
-        elif config.output['output_unit'] == 'Jy':
+        elif config.output["output_unit"] == "Jy":
             y_model = y_model * u.W / (u.m * u.m)
             y_model = y_model / ((x_model * u.um).to(u.Hz, equivalencies=u.spectral()))
             y_model = y_model.to(u.Jy).value
@@ -172,16 +207,32 @@ def single_fig():
         fig, ax1 = plt.subplots(1, 1, sharex=True, sharey=True, figsize=(8, 5))
         ax1.set_xlim(-0.99, 2.49)
         ax1.set_ylim(np.median(y_model) - 3, np.median(y_model) + 3)
-        ax1.scatter(x_data, y_data, c='blue', label='data')
-        ax1.plot(x_model, y_model, c='k', linewidth=0.5, linestyle='--', zorder=2, label='model')
-        ax1.annotate(target_name.replace('_', ' '), (0.07, 0.85), xycoords='axes fraction', fontsize=14)
-        ax1.get_xaxis().set_tick_params(which='both', direction='in', labelsize=15)
-        ax1.get_yaxis().set_tick_params(which='both', direction='in', labelsize=15)
-        ax1.set_xlabel('log $\lambda$ ($\mu m$)', labelpad=10)
+        ax1.scatter(x_data, y_data, c="blue", label="data")
+        ax1.plot(
+            x_model,
+            y_model,
+            c="k",
+            linewidth=0.5,
+            linestyle="--",
+            zorder=2,
+            label="model",
+        )
+        ax1.annotate(
+            target_name.replace("_", " "),
+            (0.07, 0.85),
+            xycoords="axes fraction",
+            fontsize=14,
+        )
+        ax1.get_xaxis().set_tick_params(which="both", direction="in", labelsize=15)
+        ax1.get_yaxis().set_tick_params(which="both", direction="in", labelsize=15)
+        ax1.set_xlabel("log $\lambda$ ($\mu m$)", labelpad=10)
         ax1.set_ylabel("log $\lambda$ F$_{\lambda}$ " + "(W m$^{-2}$)", labelpad=10)
         plt.subplots_adjust(wspace=0, hspace=0)
-        fig.savefig('output_sed'+str(target_name)+'.png', dpi=200, bbox_inches='tight')
+        fig.savefig(
+            "output_sed" + str(target_name) + ".png", dpi=200, bbox_inches="tight"
+        )
         plt.close()
 
-# if __name__ == '__main__':
-#     create_fig()
+
+if __name__ == "__main__":
+    create_fig()