e35268151f9e2b5ba8b3e915c0a7e7cd4c5a7354
Steve Goldman <sgoldman@stsci.edu>
2020-02-13 10:32:11 -0500

    tidying up code

--- desk/config.py
+++ desk/config.py
@@ -12,7 +12,7 @@ target = dict(distance_in_kpc=50, assumed_gas_to_dust_ratio=200, input_unit="Jy"
 # model options
 # Built-in options: Zubko-Crich-aringer, Oss-Orich-bb, Oss-Orich-aringer, Crystalline-20-bb, corundum-20-bb
 # Padova options: J400, J1000, H11, R12, R13
-# Grams options:
+# Grams options: grams-oxygen, grams-carbon
 
 fitting = dict(
     model_grid="Zubko-Crich-bb",
