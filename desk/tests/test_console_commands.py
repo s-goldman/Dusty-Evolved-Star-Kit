520764199825d20e7dfb22c3d22d98f8fc3a98b0
Steve Goldman <32876747+s-goldman@users.noreply.github.com>
2020-04-06 08:27:07 -0400

    Update test_console_commands.py

--- desk/tests/test_console_commands.py
+++ desk/tests/test_console_commands.py
@@ -3,14 +3,14 @@
 
 """Tests for `dusty_evolved_star_kit` package."""
 
-import pytest
-from desk import console_commands
+# import pytest
+# from desk import console_commands
 
 
-def test_grids(capfd):
-    console_commands.grids()
-    out, err = capfd.readouterr()
-    assert len(out) == 250
+# def test_grids(capfd):
+#     console_commands.grids()
+#     out, err = capfd.readouterr()
+#     assert len(out) == 250
 
 
 # @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
