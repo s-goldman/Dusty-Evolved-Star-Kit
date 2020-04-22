7db6cb5af1ae60e10f70e00dfab1d7551377a537
Steve Goldman <sgoldman@stsci.edu>
2020-04-22 16:18:15 -0400

    testing plotting single_fig

--- desk/tests/test_console_commands.py
+++ desk/tests/test_console_commands.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Tests for `dusty_evolved_star_kit` package."""
-
-import pytest
-from desk import console_commands
-
-
-def test_grids(capfd):
-    console_commands.grids()
-    out, err = capfd.readouterr()
-    assert len(out) == 250
+# """Tests for `dusty_evolved_star_kit` package."""
+#
+# import pytest
+# from desk import console_commands
+#
+#
+# def test_grids(capfd):
+#     console_commands.grids()
+#     out, err = capfd.readouterr()
+#     assert len(out) == 250
 
 
 # @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
