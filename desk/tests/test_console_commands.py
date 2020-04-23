9e68ccfa02f2ad7b2966f4e7c9b8ae18dc53833f
Steve Goldman <sgoldman@stsci.edu>
2020-04-23 16:38:55 -0400

    working regression tests for both fitting and creating sed png

--- desk/tests/test_console_commands.py
+++ desk/tests/test_console_commands.py
@@ -1,5 +1,7 @@
 from astropy.table import Table
+from PIL import Image, ImageChops
 from desk import console_commands
+import numpy as np
 
 
 def test_grids(capfd):
@@ -11,15 +13,30 @@ def test_grids(capfd):
 # @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
 # def test_get_model(test_input, expected):
 # 	assert
-
-
-def test_fit(tmpdir):
+def create_sample_data(dir):
     target_filename = "sample_target.csv"
-    target_file_path = tmpdir.join(target_filename)
+    target_file_path = dir.join(target_filename)
     file = open(target_file_path, "w")
     file.write("3.55,0.389\n4.49,0.357\n5.73,0.344\n7.87,0.506\n23.7,0.676")
     file.close()
-    console_commands.fit(source=str(target_file_path))
-    a = Table.read("fitting_results.csv")
-    b = Table.read("desk/tests/expected_fitting_results.csv")
-    assert [a[col] == b[col] for col in a.colnames[:-1]]
+    return target_file_path
+
+
+def test_single_fit(tmpdir):
+    example_filename = create_sample_data(tmpdir)
+    console_commands.fit(source=str(example_filename))
+    results = Table.read("fitting_results.csv")
+    expected_results = Table.read("desk/tests/expected_fitting_results.csv")
+    cols = results.colnames
+    np.testing.assert_allclose(
+        np.array(results[cols[2:-1]]).tolist(),
+        np.array(expected_results[cols[2:-1]]).tolist(),
+        err_msg=("Fitting results error"),
+    )
+
+
+def test_sed(tmpdir):
+    console_commands.sed()
+    sed = Image.open("output_sed.png")
+    expected_sed = Image.open("desk/tests/expected_sed.png")
+    assert ImageChops.difference(sed, expected_sed).getbbox() is None
