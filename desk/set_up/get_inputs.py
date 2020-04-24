4724ea928eba1a576a2bec8a38753a369036c9d2
Steve Goldman <sgoldman@stsci.edu>
2020-04-24 16:20:06 -0400

    cleaned up set_up.get_data

--- desk/set_up/get_inputs.py
+++ desk/set_up/get_inputs.py
@@ -3,10 +3,22 @@ __all__ = ["users"]
 
 
 class users(object):
-
-    """Docstring for users."""
+    """Initializes user inputs."""
 
     def __init__(self, source, distance, grid):
+        """
+        Initializes user inputs.
+
+        Parameters
+        ----------
+        source : str
+            user input source directory/file.
+        distance : int
+            user input distance in kpc.
+        grid : str
+            user input grid name to use
+
+        """
         # Initialize user-inputted data
         self.source = source
         self.distance = distance
