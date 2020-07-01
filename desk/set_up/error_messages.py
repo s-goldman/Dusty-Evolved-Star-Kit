debd4c2dd39a3066ab1e41c78fd15e878248cf74
Steve Goldman <sgoldman@stsci.edu>
2020-07-01 14:00:36 -0400

    get data table names = []

---
+++ desk/set_up/error_messages.py
@@ -0,0 +1,13 @@
+# Steve Goldman, Space Telescope Science Institute, sgoldman@stsci.edu
+
+
+class Fitting_Range_Error(ValueError):
+    pass
+
+
+class BadFilenameError(ValueError):
+    pass
+
+
+class BadSourceDirectoryError(ValueError):
+    pass
