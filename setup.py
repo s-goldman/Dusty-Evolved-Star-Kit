8e59c0dd4325d3f66e5f55a8ef799a511190e5d7
Steve Goldman <sgoldman@stsci.edu>
2020-07-01 12:02:36 -0400

    fixing submodule import issue

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite="tests",
     tests_require="pytest",
     url="https://github.com/s-goldman/Dusty_Evolved_Star_Kit",
-    version="1.6.20",
+    version="1.6.21",
     zip_safe=False,
 )
