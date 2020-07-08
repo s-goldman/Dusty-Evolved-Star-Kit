added nanni models. Models work but the distance scaling is still incorrect.

desk/console_commands.py
@@ -1,6 +1,7 @@
 # Steve Goldman, Space Telescope Science Institute, sgoldman@stsci.edu
-# from time import time
 import ipdb
+import math
+from astropy.table import Column
 
 from multiprocessing import Process, Value, Manager, Pool, cpu_count
 from functools import partial
@@ -52,17 +53,28 @@ def fit(
         Distance to source(s) in kiloparsecs.
     grid : str
         Name of model grid.
+    n : int
+        Number of times to scale the grid between the
+        lum_min and lum_max specified in the config.py script
+        (essentially grid density).
+    min_wavelength : float
+        Minimum wavelength to fit, the other data will still be shown
+        in the output SED.
+    max_wavelength : float
+        Maximum wavelength to be fit.
+    multiprocessing : bool
+        Flag that, if true, uses all but one of the user's
+        available cores for the fitting
+    testing : bool
+        Flag for testing that uses only the first 3 rows of the mode grids.
     """
 
     # Set-up ###################################################################
+    # bayesian fitting currently in development
     bayesian_fit = False
 
     # get data filenames
     file_names = get_data.compile_data(source)
-
-    # remove old / create new output files
-    create_output_files.remove_old_output_files()
-    create_output_files.make_output_files_dusty()
 
     # gets models
     grid_dusty, grid_outputs = get_models.get_model_grid(grid, testing)
@@ -71,6 +83,15 @@ def fit(
     if grid in config.nanni_grids:
         full_model_grid = grid_dusty
         full_outputs = grid_outputs
+        distance_norm = math.log10(((float(distance) / 4.8482e-9) ** 2) / 1379)
+
+        # model_id starts at 1
+        full_outputs.add_column(
+            Column(np.arange(1, len(full_outputs) + 1), name="model_id"), index=0
+        )
+        full_outputs.add_column(
+            Column([distance_norm] * len(full_outputs), name="norm")
+        )
         full_outputs.rename_columns(
             ["L", "vexp", "dmdt_ir"], ["lum", "scaled_vexp", "scaled_mdot"]
         )
@@ -85,9 +106,6 @@ def fit(
     #     full_outputs.add_column(Column([0] * len(full_outputs), name="scaled_vexp"))
 
     else:
-        # update ids to number in grid
-        grid_outputs["number"] = np.arange(0, len(grid_outputs))
-
         # create scaling factors for larger model grid
         scaling_factors = create_full_grid.generate_scaling_factors(distance, int(n))
 
@@ -119,6 +137,9 @@ def fit(
         testing,
     )
 
+    # remove old / create new output files
+    create_output_files.make_output_files_dusty(fit_params)
+
     if multiprocessing == True:
         # Multi-core fitting
         pool = Pool(processes=cpu_count() - 1)
@@ -129,7 +150,7 @@ def fit(
         [dusty_fit.fit_single_source(x, fit_params) for x in range(len(file_names))]
 
     # creates sed figure
-    plotting_seds.create_fig()
+    # plotting_seds.create_fig()
 
 
 def sed():

desk/set_up/get_models.py
@@ -10,10 +10,26 @@ from desk.set_up import config
 
 
 def read_hdf5(filename, testing):
+    """Reads HDF5 file.
+
+    Parameters
+    ----------
+    filename : str
+        name of the file to be read including the full path.
+    testing : str
+        If testing flag enabled, the function will only return the
+        first couple columns of the table.
+
+    Returns
+    -------
+    out: astropy table
+        contents of table at filename.
+
+    """
     with h5py.File(filename, "r") as f:
         key = list(f.keys())[0]
-        if testing == True:
-            data = list(f[key][1:4])
+        if (testing == True) | (testing == "True"):
+            data = list(f[key][0:3])
         else:
             data = list(f[key])
     out = Table(np.array(data))
@@ -75,44 +91,7 @@ def get_remote_models(model_grid_name):
         "Zubko-Crich-aringer": "https://stsci.box.com/shared/static/0nrjesla9jgxh8ioatnmrmrc5tkcf3pf.hdf5",
         "Zubko-Crich-bb": "https://stsci.box.com/shared/static/d0651w7ztavoiir7341lda8xzbwrfq9g.hdf5",
     }
-    # grid_csv_link = [
-    #     "https://stsci.box.com/shared/static/jxtabj6h5zi7a8ggb0mbtag3v6b4b9i7.csv",
-    #     "https://stsci.box.com/shared/static/z0hnvo0hz0qxcwlcvbmf01avdjlhewah.csv",
-    #     "https://stsci.box.com/shared/static/j7f01dwbsbluq4htdwlsx3v0jknye883.csv",
-    #     "https://stsci.box.com/shared/static/o6dp1cyj86jw2ifuuco983kgb2uf16sf.csv",
-    #     "https://stsci.box.com/shared/static/fuhrcgo4vh4m7ca121p1f0vuss4vszcz.csv",
-    #     "https://stsci.box.com/shared/static/he92pt1yov18x6ska5f4f4q94gal6tr5.csv",
-    #     "https://stsci.box.com/shared/static/pnljezbu8c7rcyorb27yh6hylk5wy2fs.csv",
-    #     "https://stsci.box.com/shared/static/x9fg8lacbbiye591tz723ilcqgcomyjh.csv",
-    #     "https://stsci.box.com/shared/static/4b9rc9zl110khexeuo8thb8r58mrl73g.csv",
-    #     "https://stsci.box.com/shared/static/4gxmapkhml7lnxsiu9dzto8a7zwnmzd5.csv",
-    #     "https://stsci.box.com/shared/static/kfaq7jqt4t69vitf1cifk5gurx7lnogn.csv",
-    #     "https://stsci.box.com/shared/static/xf6rrfnp1jdin94iwhwb47cxoplu3aux.csv",
-    #     "https://stsci.box.com/shared/static/5zqshpzw748n0doykm7d5cxnlvhue4kk.csv",
-    #     "https://stsci.box.com/shared/static/x7328lqy8fyg19wswqwq4md4gm398ewi.csv",
-    #     "https://stsci.box.com/shared/static/8j73qttzj10eu5sct0ds0835uxbxu3s8.csv",
-    #     "https://stsci.box.com/shared/static/ldocm4yto4vtxfcg21o23a8uilb3a3do.csv",
-    #     "https://stsci.box.com/shared/static/iy6dsqrnhi96ecnlsozo1ljr5g8b1jwx.csv",
-    # ]
-    # grid_fits_link = [
-    #     "https://stsci.box.com/shared/static/1suiv6nqbc7yoqutli2q9q98ste31o1y.fits",
-    #     "https://stsci.box.com/shared/static/bq384o6m1xm41bkx7s8oloohqkd87hy8.fits",
-    #     "https://stsci.box.com/shared/static/faekj3usdme9go3lmynga3uk0oln8v3k.fits",
-    #     "https://stsci.box.com/shared/static/w0e3f1a0r886u17vjosk8lvqrnvnw6ce.fits",
-    #     "https://stsci.box.com/shared/static/e0nvigaed722a67043rvmqhjzkonttp4.fits",
-    #     "https://stsci.box.com/shared/static/60sns7ua91ixuz4rddul2ufrmc0pu5yo.fits",
-    #     "https://stsci.box.com/shared/static/hr113z7lgvggyybh9eygi6nj69gu1gzv.fits",
-    #     "https://stsci.box.com/shared/static/hvgfnug5xxcepcz083cnrizsukgddttw.fits",
-    #     "https://stsci.box.com/shared/static/c5gdw6o3b96kaphe07y459zd7ba6590f.fits",
-    #     "https://stsci.box.com/shared/static/b78ks7cjzenagoznqqrdfoyh6bonw2sy.fits",
-    #     "https://stsci.box.com/shared/static/lw6gp2s8surgbbtmcnyev673faxqg9h6.fits",
-    #     "https://stsci.box.com/shared/static/yq6pbvepyt8a420ealg6i111xc7hcty5.fits",
-    #     "https://stsci.box.com/shared/static/qjuq780xr0ihj9p909wmijwkpclu2njd.fits",
-    #     "https://stsci.box.com/shared/static/ht6edjsrupwhytwuwa9hjg53dof2zzy0.fits",
-    #     "https://stsci.box.com/shared/static/n1ng9f4s8s7ps0ah8vp24gk0fzsxp0aj.fits",
-    #     "https://stsci.box.com/shared/static/l1qayndbz40lwewvlpg1qxu22rs9f9xu.fits",
-    #     "https://stsci.box.com/shared/static/jho46vam6d0k9jwg78gxx4eh7emwx4oz.fits",
-    # ]
+
     if model_grid_name in models:
         url_outputs = outputs[model_grid_name]
         url_models = models[model_grid_name]
@@ -177,6 +156,9 @@ def get_model_grid(grid, testing):
     ----------
     grid : str
         Model grid name.
+
+    testing: str
+        Flag for testing that returns small grid (3 rows)
 
     Returns
     -------