824dd8225c8b31e394529ce1f27075f7db784c9e
Steve Goldman <sgoldman@stsci.edu>
2020-10-05 15:53:20 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=["pytest", "sphinx_automodapi"],
     url="https://github.com/s-goldman/Dusty_Evolved_Star_Kit",
-    version="1.7.2",
+    version="1.8.0",
     zip_safe=False,
 )
