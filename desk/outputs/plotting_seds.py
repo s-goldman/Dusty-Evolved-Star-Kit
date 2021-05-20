Added distance and grid_name to scale_by_distance function in scale_external.py. Also fixed the scaling for GRAMS. Previously the model grids were scaling by the shape of the model and the luminosity, as the scaling is done for the DUSTY and Nanni et al. models. Now the grams models scale by the same norm factor but it is calculated differently for GRAMS and the nanni et al. models.

desk/outputs/plotting_seds.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import ipdb
 import seaborn as sns
 import astropy.units as u
 import matplotlib.pyplot as plt
@@ -113,20 +114,11 @@ def get_model_and_data_for_plotting(counter, target, source_path, source_filenam
 
     x_model = x_model[np.where(y_model != 0)]
     y_model = y_model[np.where(y_model != 0)]
-
-    if fnmatch(input_file["grid_name"][0], "grams*"):
-        y_model = y_model * u.Jy
-        y_model = y_model.to(
-            u.W / (u.m * u.m), equivalencies=u.spectral_density(x_model * u.um)
-        )
-
-    else:
-        y_model = y_model * u.W / (u.m * u.m)
-        y_model = y_model * np.power(10, input_file[counter]["norm"])
+    y_model = y_model * np.power(10, input_file[counter]["norm"])
 
     # logscale
     x_model = np.log10(x_model)
-    y_model = np.log10(y_model.value)
+    y_model = np.log10(y_model)
     x_data = np.log10(x_data)
     y_data = np.log10(y_data)
     return x_model, y_model, x_data, y_data

desk/set_up/scale_external.py
@@ -1,18 +1,24 @@
+import ipdb
 import numpy as np
+from fnmatch import fnmatch
 from astropy.table import Column, Table
 
 
-def scale_by_distance(_outputs, _models, scaling_factor):
+def scale_by_distance(_grid_name, _outputs, _models, scaling_factor, distance):
     """Scale external model fluxes and normalization value.
 
     Parameters
     ----------
+    _grid_name : str
+        name of grid specified
     _outputs : astropy table
         grid outputs.
     _models : astropy table
         grid models.
     scaling_factor : float
         Scaling factor to scale grids to distance.
+    distance :
+        distance in kpc
 
     Returns
     -------
@@ -22,18 +28,35 @@ def scale_by_distance(_outputs, _models, scaling_factor):
         astropy table with sclaed fluxes
 
     """
-    # get log normalization for model
-    _outputs["norm"] = np.log10(_outputs["norm"] / scaling_factor * _outputs["lum"])
 
-    # scale grid_fluxes
-    scaled_models = []
     print("Scaling model grid by distance (" + str(len(_models)) + " models)")
 
-    for i, lum_val in enumerate(_outputs["lum"]):
-        scaled_flux_array = np.multiply(
-            _models["flux_wm2"][i], lum_val / scaling_factor
+    if fnmatch(_grid_name, "grams*"):
+        # Norm is the log normalization for plotting GRAMS
+        _outputs["norm"] = np.log10(
+            _outputs["norm"] * np.square(50) / np.square(distance)
         )
-        scaled_models.append(scaled_flux_array)
+        scaled_models = _models["flux_wm2"] * np.power(10, _outputs["norm"][0])
+        if (distance < 20) | (distance > 150):
+            print(
+                "\n"
+                + "=" * 75
+                + "\nWARNING:\n"
+                + "\nThis is beyond the suggested range (20-150 kpc) "
+                + "for using the GRAMS grids.\n"
+                + "This may result in unrealistic geometries.\n\n"
+                + "=" * 75
+            )
+
+    else:
+        # Norm is the log normalization for plotting Nanni et al. models
+        _outputs["norm"] = np.log10(_outputs["norm"] / scaling_factor * _outputs["lum"])
+        scaled_models = []
+        for i, lum_val in enumerate(_outputs["lum"]):
+            scaled_flux_array = np.multiply(
+                _models["flux_wm2"][i], lum_val / scaling_factor
+            )
+            scaled_models.append(scaled_flux_array)
 
     full_models = Table(
         (_models["wavelength_um"], Column(scaled_models, name="flux_wm2"))