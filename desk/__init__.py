524fa5dd9150c1ed6c221ffc4293132ee9eaffbf
steve.goldman.astro@gmail.com <>
2021-09-17 15:50:58 -0400

    Removed aringer models, added yml for automatic upload to pypi.

--- desk/__init__.py
+++ desk/__init__.py
@@ -10,7 +10,7 @@
 __author__ = "Steven R. Goldman"
 __email__ = "sgoldman@stsci.edu"
 __title__ = "DESK"
-__version__ = "1.8.3"
+__version__ = "1.8.4"
 __repository__ = "https://github.com/s-goldman/Dusty-Evolved-Star-Kit"
 __date_published = "2021-04-13"
 __licence__ = "BSD"
